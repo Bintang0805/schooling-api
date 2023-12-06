40892462bf3600d29c054aa8fa8b8b5a6af011aa
azizikri <azizikri@gmail.com>
2023-12-07 06:40:22 +0700

    zoom

--- app/Http/Controllers/Admin/MeetingController.php
+++ app/Http/Controllers/Admin/MeetingController.php
@@ -48,14 +48,14 @@ class MeetingController extends Controller {
             "pre_schedule" => false,  // set true if you want to create a pre-scheduled meeting
             // "schedule_for" => 'ikhsanbintang1105@gmail.com', // set your schedule for
             "settings" => [
-                'join_before_host' => false, // if you want to join before host set true otherwise set false
+                'join_before_host' => true, // if you want to join before host set true otherwise set false
                 'host_video' => false, // if you want to start video when host join set true otherwise set false
                 'participant_video' => false, // if you want to start video when participants join set true otherwise set false
                 'mute_upon_entry' => true, // if you want to mute participants when they join the meeting set true otherwise set false
                 'waiting_room' => false, // if you want to use waiting room for participants set true otherwise set false
                 'audio' => 'both', // values are 'both', 'telephony', 'voip'. default is both.
                 'auto_recording' => 'none', // values are 'none', 'local', 'cloud'. default is none.
-                'approval_type' => 1, // 0 => Automatically Approve, 1 => Manually Approve, 2 => No Registration Required
+                'approval_type' => 2, // 0 => Automatically Approve, 1 => Manually Approve, 2 => No Registration Required
             ],
 
         ]);
